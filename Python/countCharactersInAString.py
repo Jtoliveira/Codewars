79e8c7076b0b89cc7f1611b37e595f97b0345d5c
Jtoliveira <joaotiagomingachos@hotmail.com>
2021-01-31 13:56:42 +0000

    some python exercise

---
+++ Python/countCharactersInAString.py
@@ -0,0 +1,19 @@
+"""
+The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+What if the string is empty? Then the result should be empty object literal, {}.
+
+"""
+def count(string):
+    # The function code should be here
+    letterDict = {}
+
+    for letter in string:
+        if letter in letterDict:
+            letterDict[letter] = letterDict.get(letter) + 1
+        else:
+            letterDict[letter] = 1 
+
+    return letterDict
+
+print(count('abab'))
